f149c9da61c7b06c2f78bcf030b58a7e2ad29d66
JiachennCJC <jiachenn99@gmail.com>
2019-04-12 11:13:06 +0800

    First commit

---
+++ Q1.m
@@ -0,0 +1,20 @@
+%Declare the population size
+pop_size = 20;
+
+%Probability of mutation of child from parent
+mutate = 0.01;
+
+%Number of simulations
+simulate = 100;
+
+%Seed for RNG
+seed = 48;
+
+%Define the payoffs
+T = 5; %Defector
+R = 3; %Mutual cooperate
+P = 1; %Defected by other player
+S = 0; %Both players defect
+
+%Fitness, using the payoff levels to determine after training
+fitness_level; 
